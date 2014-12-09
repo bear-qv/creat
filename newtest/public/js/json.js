8dd14df6d32885b92410f8c9ade2fc4576ef2e3b
MiguelValentine <460097093@qq.com>
2014-12-09 15:47:17 +0800

    del

--- newtest/public/js/json.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $('.button').click(function(){
-    $.post('/json',{'aaa':'bbb','xxx':'asdasd'},function(data){
-      
-    })
-  })
-})
--- newtest/public/js/lib/ie.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-  window.location.href="/ie";
-})
